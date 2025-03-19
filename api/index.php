7bd85f80699bf83d13fea28081555af5b946fa7b
dwikiputra18 <dwikiputra678@gmail.com>
2025-03-19 20:08:18 +0700

    vercel upload

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Illuminate\Http\Request;
-
-$request = Request::capture();
-$kernel = app()->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
